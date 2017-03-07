01df379417baf339f41c08df20535f65d1c3f569
sozysozbot <sozysozbot@gmail.com>
2017-03-07 10:59:09 +0900

    Implement cook' -- but still fails at "sashimi"

--- Pattern_match.hs
+++ Pattern_match.hs
@@ -15,7 +15,7 @@ cook :: String -> String
 cook str = concat $ map (fromJust . snd) $ cook' $ map (\x -> ([x], Nothing)) str
 
 cook' :: Stat -> Stat
-cook' = undefined
+cook' stat = foldl (flip apply) stat rls
 
 --  apply rule5 sashimi == [("s",Nothing),("a",Nothing),("s",Nothing),("h",Nothing),("i",Just "i"),("m",Nothing),("i",Just "i")]
 --  apply rule2 sashimi == [("s",Nothing),("a",Nothing),("sh",Just "\643"),("i",Nothing),("m",Nothing),("i",Nothing)]
@@ -68,19 +68,22 @@ takeTill str (x@(s,_):xs)
  | s `isPrefixOf` str = (x:) <$> takeTill (drop(length s)str) xs
  | otherwise = Nothing
 
-
+rls :: [Rule]
+rls = [rule2, rule8, rule11, rule1, rule3, rule4, rule5, rule6, rule7, rule9, rule10, rule12]
 rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8, rule9, rule10, rule11, rule12 :: Rule
-rule1 = [Right("t", W"t")]
+
 rule2 = [Right("sh", W"ʃ")]
+rule8 = [Right("x",Dollar_), Right("i", W""), Right("e",Dollar_)]
+rule11= [Right("s",W"s"), Left noVowel] 
+
+rule1 = [Right("t", W"t")]
 rule3 = [Right("s", W"z")]
 rule4 = [Right("a",W"a")]
 rule5 = [Right("i",W"i")]
 rule6 = [Right("m",W"m")]
 rule7 = [Right("s",W"s")]
-rule8 = [Right("x",Dollar_), Right("i", W""), Right("e",Dollar_)]
 rule9 = [Right("x", W"ʃ")]
 rule10= [Right("e",W"e")]
-rule11= [Right("s",W"s"), Left noVowel] 
 rule12= [Right("o",W"o")]
 
 noVowel :: Condition
