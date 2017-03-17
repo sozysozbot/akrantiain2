7ae53e79266242a9e015d34651c873290172319b
sozysozbot <sozysozbot@gmail.com>
2017-03-18 01:31:43 +0900

    write some function

--- Akrantiain/Resolve_modules.hs
+++ Akrantiain/Resolve_modules.hs
@@ -16,4 +16,19 @@ modulesToFunc :: Set Module -> Either SemanticError (Input -> Output)
 modulesToFunc [Module(ModuleName (Id "_Main"))(Sents sents)] = sentsToFunc sents
  -- FIXME
 
+liftLeft :: (a -> a) -> (Either a b -> Either a b)
+liftLeft f (Left a) = Left $ f a
+liftLeft f x = x
 
+
+moduleToModule' :: Module -> Module'
+moduleToModule' Module {moduleName = name, insideModule = Sents sents}
+ = Module'{moduleName' = name, insideModule' = Func (liftLeft f $ sentsToFunc sents)} where
+  f :: SemanticError -> SemanticError
+  f e = e{errStr = "Inside module "++ toSource name ++ ":\n"++ errStr e}  
+moduleToModule' Module {moduleName = name, insideModule = ModuleChain chain}
+ = Module'{moduleName' = name, insideModule' = ModuleChain' chain}
+
+ 
+data Module' = Module' {moduleName' :: ModuleName, insideModule' :: InsideModule'}
+data InsideModule' = Func (Either SemanticError (Input -> Output)) | ModuleChain' [ModuleName]
