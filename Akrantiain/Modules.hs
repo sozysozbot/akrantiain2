e150bb1b0a8593414212f5600a0eb085ef91c351
sozysozbot <sozysozbot@gmail.com>
2017-03-17 22:13:20 +0900

    Add parser "modules", which is incomplete and can currently only handle _Main

---
+++ Akrantiain/Modules.hs
@@ -0,0 +1,12 @@
+{-# OPTIONS -Wall -fno-warn-unused-do-bind #-}
+
+module Akrantiain.Modules
+(Module(..)
+,ModuleName(..)
+,InsideModule(..)
+) where
+import Prelude hiding (undefined)
+import Akrantiain.Structure
+data Module = Module {moduleName :: ModuleName, insideModule :: InsideModule}
+data ModuleName = Arrow {before :: Identifier, after :: Identifier} | ModuleName Identifier
+data InsideModule = Sents [Sentence] | ModuleChain [ModuleName]
