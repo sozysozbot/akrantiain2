f3fa9b28816b306759afc9d31f6d714f0251f78e
sozysozbot <sozysozbot@gmail.com>
2017-03-20 13:21:29 +0900

    useful type synonym

--- Akrantiain/Modules.hs
+++ Akrantiain/Modules.hs
@@ -4,12 +4,14 @@ module Akrantiain.Modules
 (Module(..)
 ,ModuleName(..)
 ,InsideModule(..)
+,ModChain
 ) where
 import Prelude hiding (undefined)
 import Akrantiain.Structure
 data Module = Module {moduleName :: ModuleName, insideModule :: InsideModule}
 data ModuleName = Arrow {before :: Identifier, after :: Identifier} | ModuleName Identifier deriving(Show, Eq, Ord)
-data InsideModule = Sents [Sentence] | ModuleChain [ModuleName]
+data InsideModule = Sents [Sentence] | ModuleChain ModChain 
+type ModChain = [ModuleName]
 
 instance ToSource ModuleName where
  toSource (ModuleName i) = toSource i
