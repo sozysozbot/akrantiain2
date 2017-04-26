135c6e932c39497e5d914a3aae7268db58118043
sozysozbot <sozysozbot@gmail.com>
2017-04-26 23:22:20 +0900

    Throw errors when the right-hand is made up completely of dollars

--- Akrantiain/Sents_to_rules.hs
+++ Akrantiain/Sents_to_rules.hs
@@ -9,7 +9,7 @@ import Prelude hiding (undefined)
 import Akrantiain.Structure
 import Akrantiain.Errors
 import Akrantiain.Rule
-import Control.Monad(forM,unless)
+import Control.Monad(forM,unless,when)
 import Akrantiain.Pattern_match
 import Data.List(group, sort)
 import qualified Data.Map as M
@@ -58,6 +58,7 @@ handle_conv defs_ conv@Conversion{lneg=left, mid=midd, rneg=right, phons=phoneme
   left'  <- traverse solve left
   right' <- traverse solve right
   midd'  <- traverse solve midd -- midd' :: [Either Boundary_ (Choose String)]
+  when (all isDollar phonemes) $ Left E{errNum = 336, errStr = "right-hand side of the following sentence consists solely of dollar(s):\n" ++ toSource conv}
   case zipEither midd' (map phonToW phonemes) of
    Nothing -> Left E{errNum = 333, errStr = "mismatched number of concrete terms in left- and right-hand side of:\n" ++ toSource conv}
    Just newmidd -> return R{leftneg = fmap no' left', middle = newmidd, rightneg = fmap no' right'}
@@ -77,6 +78,10 @@ resolveSelect _ (Pipe(Ch quotes)) = return $ (Right . Ch . map unQ) quotes
 resolveSelect defs (Iden iden) = case iden `M.lookup` defs of
  Nothing -> Left E{errNum = 335, errStr = "unresolved identifier {" ++ unId iden ++ "}"}
  Just (Ch quotes) -> return $ (Right . Ch . map unQ) quotes
+
+isDollar :: Phoneme -> Bool
+isDollar Dollar = True
+isDollar _ = False
 
 phonToW :: Phoneme -> W
 phonToW Dollar = Dollar_
