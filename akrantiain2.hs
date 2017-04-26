c2b3a2c385575ca6c841bfcdc68056990db50aa6
sozysozbot <sozysozbot@gmail.com>
2017-04-26 22:47:08 +0900

    Ver 0.5.10
    Warn unknown setting specifiers

--- akrantiain2.hs
+++ akrantiain2.hs
@@ -54,4 +54,4 @@ interact' f = do
    Left a -> hPrint stderr a >> putStrLn ""
 
 explain :: IO ()
-explain = putStrLn "akrantiain (ver 0.5.9)\na domain-specific language designed to describe conlangs' orthographies" -- FIXME: better explanation required
+explain = putStrLn "akrantiain (ver 0.5.10)\na domain-specific language designed to describe conlangs' orthographies" -- FIXME: better explanation required
