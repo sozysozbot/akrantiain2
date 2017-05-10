863ae6fb7f1fb9f40ed9cd4eda30374c26d8b563
sozysozbot <sozysozbot@gmail.com>
2017-05-11 01:36:35 +0900

    Write “check” in tester.hs

--- tester.hs
+++ tester.hs
@@ -3,7 +3,7 @@ import System.IO
 import System.Process
 import System.Info
 import Control.Monad
-
+import Control.Exception as E
 
 
 main :: IO ()
@@ -14,7 +14,18 @@ main = do
    hPutStrLn stderr "Usage: ./tester create [sample_names]"
    void getLine
   ("create":arr) -> forM_ arr $ \name -> do
-   hPutStrLn stderr $ "Creating output sample for {" ++ name ++ "}..."
+   hPutStrLn stderr $ "Creating the output sample for {" ++ name ++ "}..."
    callCommand $ 
     "./akrantiain2 samples/sample_" ++ name ++ ".snoj < samples/input_sample_" ++ name ++ ".txt > samples/output_sample_" ++ name ++ ".txt"
-   hPutStrLn stderr $ "Created output sample for {" ++ name ++ "}."
+   hPutStrLn stderr $ "Created the output sample for {" ++ name ++ "}."
+  ("check":arr) -> forM_ arr $ \name -> do
+   hPutStrLn stderr $ "Checking the output of sample {" ++ name ++ "}..."
+   callCommand $ 
+    "./akrantiain2 samples/sample_" ++ name ++ ".snoj < samples/input_sample_" ++ name ++ ".txt > samples/output_sample_" ++ name ++ ".tmp"
+   callCommand ("diff samples/output_sample_" ++ name ++ ".tmp samples/output_sample_" ++ name ++ ".txt") `E.catch` foo name
+   hPutStrLn stderr $ "Finished checking the output of sample {" ++ name ++ "}."
+
+foo :: String -> E.IOException -> IO a
+foo name e = do
+    hPutStrLn stderr $ "FAILURE WHILE CHECKING THE OUTPUT OF SAMPLE {" ++ name ++ "}."
+    throwIO e
