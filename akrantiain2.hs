514a09636b7fcdc7790bb3d372d76b48552848c1
sozysozbot <sozysozbot@gmail.com>
2017-05-29 16:38:55 +0900

    ver 0.6.4
    Add many samples
    Fix bug: snoj file was NFD'd even when there isn't @ USE_NFD;

--- akrantiain2.hs
+++ akrantiain2.hs
@@ -54,4 +54,4 @@ interact' f = do
    Left a -> hPrint stderr a >> putStrLn ""
 
 explain :: IO ()
-explain = putStrLn "akrantiain (ver 0.6.3)\na domain-specific language designed to describe conlangs' orthographies" -- FIXME: better explanation required
+explain = putStrLn "akrantiain (ver 0.6.4)\na domain-specific language designed to describe conlangs' orthographies" -- FIXME: better explanation required
