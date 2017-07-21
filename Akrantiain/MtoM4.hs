f72b7efa885848a30b7d35f4501b00dc561583bc
sozysozbot <sozysozbot@gmail.com>
2017-07-21 17:02:53 +0900

    Define moduleToModule2, but realizes that it can be generalized

--- Akrantiain/MtoM4.hs
+++ Akrantiain/MtoM4.hs
@@ -1,6 +1,8 @@
 {-# OPTIONS -Wall -fno-warn-unused-do-bind #-}
 module Akrantiain.MtoM4
 (moduleToModule4
+,moduleToModule2
+,Module2
 ,Module4
 ,InsideModule4
 ) where
@@ -14,12 +16,32 @@ type Module4 = Module_ (Input -> Output)
 -- data Module4 = Module4 {moduleName4 :: ModuleName, insideModule4 :: InsideModule4}
 type InsideModule4 = InsideModule_ (Input -> Output) 
 
+type Module2 = Module_ SanitizedSentences
+
 liftLeft :: (a -> c) -> (Either a b -> Either c b)
 liftLeft f (Left a) = Left $ f a
 liftLeft _ (Right x) = Right x
 
 liftLeft2 :: (a -> c) -> (WriterT d (Either a) b -> WriterT d (Either c) b)
 liftLeft2 f = WriterT . liftLeft f . runWriterT
+
+mToM :: (a -> SemanticMsg b) -> Module_ a -> SemanticMsg (Module_ b)
+mToM fun Module{moduleName = name, insideModule = Sents sents} = do
+ new <- liftLeft2 f $ fun sents
+ return Module{moduleName = name, insideModule = Sents new} where
+  f :: SemanticError -> SemanticError
+  f e = e{errStr = "Inside module "++ toSource name ++ ":\n"++ errStr e}
+mToM _ Module {moduleName = name, insideModule = ModuleChain chain}
+ = return Module{moduleName = name, insideModule = ModuleChain chain}
+
+moduleToModule2 :: Module -> SemanticMsg Module2
+moduleToModule2 Module {moduleName = name, insideModule = Sents sents} = do
+ sanitized <- liftLeft2 f $ sanitizeSentences sents
+ return Module{moduleName = name, insideModule = Sents sanitized} where
+  f :: SemanticError -> SemanticError
+  f e = e{errStr = "Inside module "++ toSource name ++ ":\n"++ errStr e}
+moduleToModule2 Module {moduleName = name, insideModule = ModuleChain chain}
+ = return Module{moduleName = name, insideModule = ModuleChain chain}
 
 moduleToModule4 :: Module -> SemanticMsg Module4
 moduleToModule4 Module {moduleName = name, insideModule = Sents sents} = do
