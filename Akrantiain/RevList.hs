63c6ad3b6d09635ae84942d5b27cd3a3e58a8adf
sozysozbot <sozysozbot@gmail.com>
2017-07-29 06:18:58 +0900

    instance Foldable RevList

--- Akrantiain/RevList.hs
+++ Akrantiain/RevList.hs
@@ -15,3 +15,12 @@ toList (Reverse a) = reverse a
 fromList :: [a] -> RevList a
 fromList bs = Reverse (reverse bs)
 
+instance Foldable RevList where
+ foldr f z (Reverse arr) = foldl (flip f) z arr
+--    foldr f z (Reverse [x1, x2, ..., xn])
+-- == foldr f z . toList $ (Reverse [x1, x2, ..., xn])
+-- == foldr f z (reverse [x1, x2, ..., xn])
+-- == foldr f z [xn, ..., x2, x1] 
+-- == xn `f` (xn_1 `f` ... (x1 `f` z)...)
+-- ==  (... (z `g` x1) ... `g` xn_1) `g` xn where g = flip f
+-- == foldl (flip f) z [x1, x2, ..., xn]
