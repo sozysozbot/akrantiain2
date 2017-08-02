2974f9cd7cca3f3fb2af2281472664072178cdd3
sozysozbot <sozysozbot@gmail.com>
2017-08-02 20:20:39 +0900

    Move files

--- tester.hs
+++ tester.hs
@@ -110,13 +110,13 @@ getSnojPath :: String -> String
 getSnojPath name = "samples/sample_" ++ name ++ ".snoj"
 
 getInputSamplePath :: String -> String
-getInputSamplePath name = "samples/input_sample_" ++ name ++ ".txt" 
+getInputSamplePath name = "samples/input_sample/input_sample_" ++ name ++ ".txt" 
 
 getOutputSamplePath :: String -> String
-getOutputSamplePath name = "samples/output_sample_" ++ name ++ ".txt" 
+getOutputSamplePath name = "samples/output_sample/output_sample_" ++ name ++ ".txt" 
 
 getJSampleTmpPath :: String -> String
-getJSampleTmpPath name = "samples/.jsample_" ++ name ++ ".tmp"
+getJSampleTmpPath name = "samples/tmp/.jsample_" ++ name ++ ".tmp"
 
 getOSampleTmpPath :: String -> String
-getOSampleTmpPath name = "samples/.output_sample_" ++ name ++ ".tmp"
+getOSampleTmpPath name = "samples/tmp/.output_sample_" ++ name ++ ".tmp"
