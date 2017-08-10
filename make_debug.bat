f41e1c43128f6538d38fa582a01cb805b02eb898
sozysozbot <sozysozbot@gmail.com>
2017-08-10 19:46:34 +0900

    Update windows bat file (though not yet tested)

--- make_debug.bat
+++ make_debug.bat
@@ -6,7 +6,8 @@ ghc --make akrantiain2
 ghc --make debug1
 ghc --make debug2
 ghc --make debug3
-ghc --make tester
-tester check lineparine lineparine2 zyepheng emptystring1
+ghc --make test\tester
+test\tester --check_from test\testerlist.txt
+test\tester --checkJSON_from test\testerlist2.txt
 pause
 exit /b
